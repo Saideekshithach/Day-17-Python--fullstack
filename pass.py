ec34a20f91277d8ba59f6691c09709170187c11c
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-11-10 18:47:34 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,19 @@
+'''for i in range(10):
+    if i==5:
+        break
+    print(i)
+for i in range(20):
+    if i==8:
+        continue
+    print(i)'''
+
+#pass
+'''for i in range(9):
+    if i==4:
+        pass
+    print(i)
+   ''' 
+for i in range(9):
+    if i==4:
+        print(i)
+    pass
